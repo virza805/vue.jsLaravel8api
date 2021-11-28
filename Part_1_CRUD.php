70a47b5db653683a8275bafbd685b9429ce74b10
virza805 <virza805@gmail.com>
2021-11-29 00:49:16 +0600

    Laravel 8 api vue.js project

---
+++ Part_1_CRUD.php
@@ -0,0 +1,22 @@
+<?php 
+
+# composer create-project laravel/laravel example-app |=> Installation Via Composer
+# composer require laravel/ui |=> https://laravel.com/docs/7.x/frontend
+# php artisan ui vue |=> 
+# npm install |=> 
+# 
+# Database cerate and .env file link pcrud
+# php artisan serve |=====> run this 
+
+# npm run watch |=> if show some problam then run this commend below
+# npm install vue-loader@^15.9.7 --save-dev --legacy-peer-deps |=> now again run
+
+# npm run watch |=====> run this
+# php artisan make:model Todo -m |=> 
+#  |=> 
+#  |=> 
+#  |=> 
+
+
+
+
